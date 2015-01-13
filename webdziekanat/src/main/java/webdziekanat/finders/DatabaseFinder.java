187ce2c3a0eecebd47dceef4a79b151b77100066
chewie <chewie@chewie-Aspire-V3-571G>
2015-01-13 21:03:56 +0100

    [webdziekanat] - brakujące pliki

---
+++ webdziekanat/src/main/java/webdziekanat/finders/DatabaseFinder.java
@@ -0,0 +1,51 @@
+package webdziekanat.finders;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Example;
+import org.springframework.stereotype.Component;
+
+import webdziekanat.model.Address;
+import webdziekanat.model.Student;
+
+@Component
+@Transactional
+public class DatabaseFinder {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+    
+    
+    public DatabaseFinder() {
+    }
+
+    public Student findStudent(Student src)
+    {
+        Session session = (Session) entityManager.getDelegate();
+        
+        Example addressExample = Example.create(src);
+        
+        Criteria criteria = session.createCriteria(Student.class).add(addressExample);
+        
+        Student foundStudent = (Student) criteria.uniqueResult();
+        
+        return foundStudent;
+    }
+
+    public Address findAddress(Address src)
+    {
+        Session session = (Session) entityManager.getDelegate();
+        
+        Example addressExample = Example.create(src);
+        
+        Criteria criteria = session.createCriteria(Address.class).add(addressExample);
+        
+        Address foundAddress = (Address) criteria.uniqueResult();
+        
+        return foundAddress;
+    }
+}
---
+++ webdziekanat/src/main/java/webdziekanat/model/Subjets.java
@@ -0,0 +1,64 @@
+package webdziekanat.model;
+
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "subjets")
+public class Subjets {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    private int etcs;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "subjects_students", joinColumns = @JoinColumn(name = "student_id"),
+            inverseJoinColumns = @JoinColumn(name = "subject_id"))
+    private Set<Student> students;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEtcs() {
+        return etcs;
+    }
+
+    public void setEtcs(int etcs) {
+        this.etcs = etcs;
+    }
+
+    public Set<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(Set<Student> students) {
+        this.students = students;
+    }
+    
+}
